29972974b9917f260bf6860bb70d1bff2eca5826
Ludovic Deparis <maldestor95@gmail.com>
2020-02-01 12:07:02 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-user></form-user>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import FormUser from './components/FormUser.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    FormUser
   }
 }
 </script>
