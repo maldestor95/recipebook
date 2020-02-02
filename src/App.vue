0090e0e5795b03e2072e78c1e73e6e0126471ccb
Ludovic Deparis <maldestor95@gmail.com>
2020-02-02 22:56:49 +0100

    Create Login Form

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,30 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+<v-app>
+    <v-app-bar app color="primary" dark>
+        <login-user color="info"></login-user>
     </v-app-bar>
- <form-user></form-user>
     <v-content>
-      <HelloWorld/>
+        <v-container justify-center>
+            <v-row>
+            </v-row>
+        </v-container>
     </v-content>
-  </v-app>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-import FormUser from './components/FormUser.vue'
-
+import LoginUser from './components/LoginUser.vue'
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-    HelloWorld,
-    FormUser
+    components: {
+        LoginUser
 
-  },
+    },
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        //
+    }),
 };
 </script>
---
+++ src/components/LoginUser.vue
@@ -0,0 +1,74 @@
+<template>
+<v-dialog v-model="dialog" persistent max-width="290">
+    <template v-slot:activator="{ on }">
+        <v-btn v-on="on">
+            <v-icon>{{islogged==true?'mdi-logout':'mdi-login'}}</v-icon>
+        </v-btn>
+    </template>
+
+    <v-card v-if="islogged">
+        <v-row>
+            <v-col md12>
+                <p>Are you sure you want to log out?</p>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col md3>
+                <v-btn color="primary" @click="logout">confirm</v-btn>
+            </v-col>
+            <v-col md9>
+                <v-btn color="primary" @click="dialog=false">
+                    <v-icon>mdi-cancel</v-icon>
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-card>
+    <v-card v-if="!islogged">
+        <v-text-field name="username" outlined label="Username or email address"></v-text-field>
+        <v-row>
+            <v-col md6>
+                <p>forgot password?</p>
+            </v-col>
+        </v-row>
+        <v-text-field name="password" outlined label="password"></v-text-field>
+        <v-btn color="primary" @click="login">Login</v-btn>
+        <v-btn color="primary" @click="dialog=false">
+            <v-icon>mdi-cancel</v-icon>
+        </v-btn>
+    </v-card>
+
+</v-dialog>
+</template>
+
+<script>
+export default {
+    props: {
+        logged: {
+            type: Boolean,
+            default: true
+        },
+    },
+    data() {
+        return {
+            islogged: this.logged,
+            dialog: false
+        }
+    },
+    methods: {
+        logout() {
+            this.dialog = false
+            this.islogged = false
+        },
+        login() {
+            this.dialog = false
+            this.islogged = true
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+vfor
