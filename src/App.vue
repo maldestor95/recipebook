094e44e2342cbac5a5b11f81eafc89db6fa02b1b
Maldestor95 <maldestor95@gmail.com>
2020-02-04 17:06:55 +0100

    addition of Store

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,79 @@
 <template>
-<v-app>
+  <v-app>
     <v-app-bar dense app color="primary" dark>
-        <v-icon @click="navdrawer=!navdrawer">mdi-menu</v-icon>
-        <v-spacer></v-spacer>
-        <h1>{{currentApp}}</h1>
-        <v-spacer></v-spacer>
-        <login-user color="info"></login-user>
+      <v-icon @click="navdrawer=!navdrawer">mdi-menu</v-icon>
+      <v-spacer></v-spacer>
+      <h1>{{currentApp}}</h1>
+      <v-spacer></v-spacer>
+      <login-user color="info"></login-user>
     </v-app-bar>
     <v-content>
-        <v-navigation-drawer temporary v-model="navdrawer">
+      <v-row>
+        <v-col fluid>
+          <v-navigation-drawer temporary v-model="navdrawer">
             <v-list dense>
-                <v-subheader>TOOLS</v-subheader>
-                
+              <v-subheader>TOOLS</v-subheader>
+              <v-list-item-group v-model="item" color="primary">
+                <v-list-item v-for="(item, i) in navlist" :key="i">
+                  <div v-if="item.logrequired">
+                    <v-list-item-icon>
+                      <v-icon v-text="item.icon"></v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+                  </div>
+                </v-list-item>
+              </v-list-item-group>
             </v-list>
-            
-        </v-navigation-drawer>
-        <v-container justify-center>
+          </v-navigation-drawer>
+        </v-col>
+
+        <v-col>
+          <v-container>
             <v-row>
+              <v-col>
+                <h1>Is Logged ?{{Store.logged}}</h1>
+              </v-col>
             </v-row>
-        </v-container>
+          </v-container>
+        </v-col>
+      </v-row>
     </v-content>
-</v-app>
+  </v-app>
 </template>
 
 <script>
-import LoginUser from './components/LoginUser.vue'
+import LoginUser from "./components/LoginUser.vue";
+import storey from "./store.js";
 
 export default {
-    name: 'App',
-
-    components: {
-        LoginUser
-
-    },
-
-    data: () => ({
-        navdrawer:null,
-        currentApp:"Current Application"
-    }),
+  name: "App",
+  components: {
+    LoginUser
+  },
+  data: () => ({
+    Store: storey.state,
+    navdrawer: null,
+    currentApp: "Current Application",
+    item: 1,
+    navlist: [
+      {
+        icon: "mdi-alert-box",
+        text: "Risks",
+        logrequired: false
+      },
+      {
+        icon: "mdi-cash-100",
+        text: "Expenses",
+        logrequired: true
+      }
+    ]
+  }),
+  computed: {
+    navlistfilter() {
+      return this.data;
+    }
+  }
 };
 </script>
