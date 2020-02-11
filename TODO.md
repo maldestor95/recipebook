d472e89e0ba544712410ff3f246520d01b67e4c9
Maldestor95 <maldestor95@gmail.com>
2020-02-11 14:04:25 +0100

    Expenses Add DatePickerMenu.vue

--- TODO.md
+++ TODO.md
@@ -1,8 +1,18 @@
 # TODO
 
-* Configuration to minimise the build (WebPack)
-* utilisation de variable d'environnement https://cli.vuejs.org/guide/mode-and-env.html#modes
+- Configuration to minimise the build (WebPack)
+- Serveur Node JS
+- Axios
+- File Upload Component
 
 # DONE
 
-* Vue Router
+## 2020-02-11
+
+- création composant DatePickerMenu
+- Gestion des appuis touche dans DatePickerMenu(esc, enter...) https://vuejs.org/v2/guide/events.html#Key-Codes
+
+## 2020-01-31
+
+- utilisation de variable d'environnement https://cli.vuejs.org/guide/mode-and-env.html#modes
+- Vue Router
---
+++ src/components/DatePickerMenu.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-text-field
+      class="text-center"
+      :name="name"
+      :label="label"
+      :id="id"
+      :value="SelectDate"
+      @focus="ShowPicker = true"
+      @blur="ShowPicker = false"
+      @keyup.esc="ShowPicker = false"
+      @keyup.enter="ShowPicker = false"
+      v-if="ShowPicker == false"
+    >
+    </v-text-field>
+    <v-dialog v-model="ShowPicker" width="500">
+      <v-date-picker
+        v-model="SelectDate"
+        type="month"
+        @blur="ShowPicker = false"
+        @change="$emit('input', SelectDate)"
+      >
+        v-if="ShowPicker == true"
+        <p align-center>Select Start Date</p>
+        <v-btn text color="primary" @click="ShowPicker = false">Quit</v-btn>
+      </v-date-picker>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    },
+    label: {
+      type: String,
+      default: "Date"
+    },
+    id: {
+      type: String,
+      default: "id"
+    },
+    name: {
+      type: String,
+      default: "name"
+    }
+  },
+  data() {
+    return {
+      SelectDate: this.value,
+      ShowPicker: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
