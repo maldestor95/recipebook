e64f0166f2e93694baf9ab4c81db76ec80a1acd6
maldestor <maldestor95@gmail.com>
2020-02-25 05:51:01 +0100

    add nodemon disable x-powered-by add verbs to REST expense API

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require("express");
+const app = express();
+const port = 3000;
+const dev = process.env.NODE_ENV !== "production";
 
 var myLogger = function (req, res, next) {
-    console.log('LOGGED')
-    next()
-}
+    console.log("LOGGED");
+    next();
+};
+app.disable("x-powered-by");
+app.use(myLogger);
 
-app.use(myLogger)
+app.get("/tot", function (req, res) {
+    res.send("Hello World!");
+});
+app.use("/API", require("./lib/expensesbdd_router"));
+app.get("/", (req, res) => res.send("Hello toto!"));
 
-app.get('/tot', function (req, res) {
-    res.send('Hello World!')
-})
-app.use('/wiki', require('./wiki/routewiki'));
-app.get('/', (req, res) => res.send('Hello toto!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
