715939f0d112e0bc8fe7889534a95f51b7be57b1
maldestor <maldestor95@gmail.com>
2020-02-26 14:12:18 +0100

    Test Expense bdd

--- test/test_Expensebdd.js
+++ test/test_Expensebdd.js
@@ -1,65 +1,115 @@
-// var assert = require("chai").assert;
-const expenseBdd = require('../lib/expensesbdd')
+const expenseBdd = require("../lib/expensesbdd");
+const fs = require("fs");
+let expenseBddData = [{}];
+var assert = require("chai").assert;
 
-let expenseBddData = [{}]
-var assert = require('chai').assert
+const tempdatabasepath = "temp/testbddExpense.json";
+
 describe("ExpenseBdd", function () {
-    before(() => {
+    beforeEach(done => {
 
-        }),
-        it("shall read BddExpenseFile", function (done) {
-            expenseBdd.readBdd(
-                (err, data) => {
-                    if (err) throw err
-                    assert.equal(typeof ({}), typeof (data))
-                    expenseBddData = data
-                    done();
-
-                }
-            )
+        fs.copyFile("temp/testbddExpenseOrig.json", tempdatabasepath, err => {
+            if (err) throw err;
+            expenseBdd.readBdd(tempdatabasepath, (err, data) => {
+                if (err) throw err;
+                assert.equal(typeof {}, typeof data);
+                expenseBddData = data;
+                done();
+            });
         });
-    it("shall write BddExpenseFile", function () {
+    });
+    afterEach(done => {
+        fs.unlink(tempdatabasepath, err => {
+            done()
+        });
+    });
+    it("shall be properly initialised", function () {
+        assert.lengthOf(expenseBddData, 5, "ExpenseBDDdata shall have 5 elements");
+        assert.equal(expenseBddData[1].desc, "tata", "verify desc on 2nd entry");
+    });
+    it("shall write BddExpenseFile", function (done) {
         const newitem = {
             id: 1,
-            desc: 'new',
-            category: 'new',
-            date: '21/12/2999',
+            desc: "new",
+            category: "new",
+            date: "21/12/2999",
             cost: 123
-        }
-        expenseBddData.push(newitem)
-        expenseBdd.writeBdd(expenseBddData, dest = 'temp/t.json')
+        };
+        expenseBddData.push(newitem);
+        expenseBdd.writeBdd(
+            expenseBddData,
+            (dest = tempdatabasepath),
+            (err, data) => {
+                done();
+            }
+        );
     });
-
-    it("Shall find item by id", (done) => {
-        expenseBdd.find({
-            "id": 2
-        }, function (err, res) {
+    describe("find items", function () {
+        it("by wrong id shall return empty array", done => {
+            expenseBdd.find({
+                    id: 10000
+                },
+                (err, res) => {
+                    assert.equal(err, null);
+                    assert.equal(res.length, 0);
+                    assert.typeOf(res, Array);
+                }
+            );
+            done();
+        });
+        it("by id", done => {
+            expenseBdd.find({
+                    id: 2
+                },
+                (err, res) => {
+                    assert.equal(err, null);
+                    assert.equal(res.length, 1);
+                    assert.deepEqual(res[0], {
+                        id: 2,
+                        desc: "tata",
+                        category: "general",
+                        date: "21/12/2019",
+                        cost: 125
+                    });
+                    done();
+                }
+            );
+        });
+        it("by description", done => {
+            expenseBdd.find({
+                    description: "tu"
+                },
+                (err, res) => {
+                    assert.lengthOf(res, 2, "shall find 2 matches")
+                    done();
+                }
+            );
+        });
+        it("by description or id", done => {
+            expenseBdd.find({
+                    description: "tu",
+                    id: 2
+                },
+                (err, res) => {
+                    assert.lengthOf(res, 3, "shall find 3 matches")
+                    done();
+                }
+            );
+        });
+    });
+    describe.skip("add item", function () {
+        it("shall ", (done) => {
             done()
         })
-
     })
-    it("Shall find item by descrition", (done) => {
-        expenseBdd.find({
-            "description": 'tata'
-        }, (err, res) => {
+    describe.skip("remove item", function () {
+        it("shall ", (done) => {
             done()
         })
-
     })
-    it("Shall find item by descrition or id", (done) => {
-        expenseBdd.find({
-            "description": 'tata',
-            "id": 4
-        }, (err, res) => {
+    describe.skip("update item", function () {
+        it("shall ", (done) => {
             done()
         })
-
     })
-    describe("ExpenseBddout", function () {
-        it("shall manage object", () => {
-            let ii = expenseBdd.bddexpenseout
-            ii.print()
-        })
-    })
-
 });
