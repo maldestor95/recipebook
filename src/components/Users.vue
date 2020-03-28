821d86ff20ad1c687a49d014543222318c50ee63
maldestor95 <maldestor95@gmail.com>
2020-03-28 14:07:27 +0100

    Users Privilege - addition possible

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -14,8 +14,8 @@
         ></v-text-field>
       </v-card-title>
 
-        <!-- show-select -->
-        <!-- single-select -->
+      <!-- show-select -->
+      <!-- single-select -->
       <v-data-table
         :headers="headers"
         :items="Users"
@@ -34,7 +34,7 @@
             </template>
             <span>Edit</span>
           </v-tooltip>
-            {{ item.login.toUpperCase() }}
+          {{ item.login.toUpperCase() }}
         </template>
       </v-data-table>
     </v-card>
@@ -43,29 +43,47 @@
 
     <v-dialog v-model="userEditDialog">
       <v-tabs v-model="tabs" color="primary" slider-color="primary">
-        <v-tab>Application</v-tab>
-        <v-tab>Details</v-tab>
-        <v-tab>Pwd</v-tab>
+        <v-tab :disabled="isDisabled">Application</v-tab>
+        <v-tab :disabled="isDisabled">Details</v-tab>
+        <v-tab :disabled="isDisabled">Pwd</v-tab>
         <v-tab>Administration</v-tab>
       </v-tabs>
 
       <v-tabs-items v-model="tabs">
         <v-tab-item>
           <!-- TODO  updateApplication-->
-          <users-app :details="details" @change="updatePwd"></users-app>
+          <users-app
+            v-model="details"
+            :appList="appList"
+            :rightsList="rightsList"
+            @change="updateApp"
+          ></users-app>
         </v-tab-item>
+
         <v-tab-item>
-    <users-details :details="details" @change="updateDetails"></users-details>
+          <users-details :details="details" @change="updateDetails"></users-details>
         </v-tab-item>
+
         <v-tab-item>
           <users-pwd :details="details" @change="updatePwd"></users-pwd>
         </v-tab-item>
+
         <v-tab-item>
-          <!-- TODO  admin-->
-          <users-admin :details="details" :toDelete="itemToDelete" @leave="userEditDialog=false;scan()"></users-admin>
+          <users-admin
+            :details="details"
+            :toDelete="itemToDelete"
+            @leave="userEditDialog=false;scan()"
+          ></users-admin>
         </v-tab-item>
       </v-tabs-items>
     </v-dialog>
+
+    <v-card>
+      <!-- <v-btn color="success" @click="getAppList()">Get App List</v-btn> -->
+      {{appList}}
+      <!-- <v-btn color="success" @click="getRightsList()">Get Rights List</v-btn> -->
+      {{rightsList}}
+    </v-card>
   </div>
 </template>
 <script>
@@ -75,6 +93,8 @@ import usersDetails from "./UsersDetails";
 import usersPwd from "./UsersPwd";
 import usersApp from "./UsersApp";
 import usersAdmin from "./UsersAdmin";
+import usersApi from "./usersapi";
+
 import qs from "qs";
 
 export default {
@@ -83,7 +103,7 @@ export default {
   data() {
     return {
       tabs: "",
-      itemToDelete:false,
+      itemToDelete: false,
       userEditDialog: false,
       search: "",
       singleSelect: false,
@@ -104,11 +124,16 @@ export default {
         email: "selectedUserEmailInitial",
         phone: "selectedUserPhoneInitial",
         address: "selectedUserAddressInitial"
-      }
+      },
+      appList: [],
+      rightsList: [],
+      isDisabled: true
     };
   },
   mounted: function() {
     this.scan();
+    this.getAppList();
+    this.getRightsList();
   },
   methods: {
     scan() {
@@ -121,15 +146,17 @@ export default {
           this.Users = err;
         });
     },
-    usersAddDialog(){
-      this.tabs=3
-      this.details={login:null}
-      this.userEditDialog=true
-      this.itemToDelete=false
+    usersAddDialog() {
+      this.isDisabled=true
+      this.tabs = 3;
+      this.details = { login: null };
+      this.userEditDialog = true;
+      this.itemToDelete = false;
     },
 
     handleClick(value) {
       this.Value = value;
+      this.isDisabled=false
       // this.$set(this.details,'login',value.login)
       this.details = {
         login: value.login,
@@ -140,7 +167,7 @@ export default {
         pwd: value.pwd,
         userApplication: value.userApplication
       };
-      this.itemToDelete=true
+      this.itemToDelete = true;
     },
     updateDetails() {
       const user = this.details.login;
@@ -192,6 +219,29 @@ export default {
             this.msg = JSON.stringify(err);
             this.showMsg = true;
           }
+        });
+    },
+    updateApp() {},
+
+    // methods used to initiate the Applist
+    getAppList() {
+      usersApi
+        .getApplicationList()
+        .then(data => {
+          this.appList = data;
+        })
+        .catch(err => {
+          this.appList = err;
+        });
+    },
+    getRightsList() {
+      usersApi
+        .getRightsList()
+        .then(data => {
+          this.rightsList = data;
+        })
+        .catch(err => {
+          this.rightsList = err;
         });
     }
   }
---
+++ src/components/UsersApp.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <h1>Edit Privileges for user ---- {{value.login}}</h1>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-select :items="appList" v-model="appListSelection" label="Application"></v-select>
+        </v-col>
+        <v-col>
+          <v-select :items="rightsList" v-model="rightsListSelection" label="Privilege"></v-select>
+        </v-col>
+        <v-col cols="1">
+          <v-btn rounded color="success" @click="addPrivilege()">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container grid-list-xs>
+      <v-row v-for="(appname,index) in value.userApplication" :key="index">
+        <v-col cols="1">{{index}}</v-col>
+        <v-col>{{Object.keys(appname)[0]}}</v-col>
+        <v-col>{{Object.values(appname)[0]}}</v-col>
+        <v-col>
+          <v-btn icon dense color="error" cols="1" @click="deletePrivilege(appname)">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-btn rounded color="primary" dark @click="sendForm">Submit</v-btn>
+{{value}}
+  </div>
+</template>
+
+<script>
+import usersApi from "./usersapi";
+export default {
+  props: {
+    appList: { type: Array },
+    rightsList: { type: Array },
+    value: {
+      type: Object,
+      default: function() {
+        return {
+          userApplication:[{ Users: "Root" }, { Todo: "Viewer" }, { Expenses: "Manager" }],
+          login:"toto",
+          version:123
+        };
+      }
+    }
+  },
+  data() {
+    return {
+      rightsListSelection: null,
+      appListSelection: null,
+      activeRow: false
+    };
+  },
+  methods: {
+    sendForm(){
+      // TODO 
+    },
+    deletePrivilege(ItemApp) {
+      this.value = this.value.filter(x => {
+        return Object.keys(x)[0] != Object.keys(ItemApp)[0];
+      });
+    },
+    addPrivilege() {
+      let privlist = this.value.userApplication;
+      let activeKeyList = this.value.userApplication.map(function(x) {
+        return Object.keys(x)[0];
+      });
+      if (activeKeyList.lastIndexOf(this.appListSelection) < 0) {
+        let newApp = {};
+        newApp[this.appListSelection] = this.rightsListSelection;
+        privlist.push(newApp);
+        this.$set(this.value.userApplication, privlist);
+        usersApi
+          .updateApplication({
+            login: this.value.login,
+            version: this.value.version,
+            userApplication: this.value.userApplication
+          })
+          .then(response => {
+            privlist = response;
+          })
+          .catch(err => {
+            privlist = err;
+          });
+      } else {
+        // TODO message because the list of user is not accessible
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.focusEntry {
+  border: pink;
+  color: red;
+}
+</style>
