9e48c14b933042c7dab81d89f930395ef77037b3
maldestor95 <maldestor95@gmail.com>
2020-03-28 15:35:12 +0100

    Users_router ongoing methods writing + relative tests methods

--- test/test_Users_router.js
+++ test/test_Users_router.js
@@ -1,50 +1,184 @@
 var assert = require("chai").assert;
 var axios = require("axios")
-var qs= require("qs")
+var qs = require("qs")
 const baseURL = "http://localhost:3000"
-describe("Tests Users router", function () {
-    let looptest = [
+describe("Users router", function () {
+    describe("scanusers", function () {
+        it('/API/Users/scan', done => {
+            data = null
+            axios.request({
+                    method: "get",
+                    url: baseURL + '/API/Users/scan',
+                    data: qs.stringify(data)
+                })
+                .then((response) => {
+                    assert.equal(response.status, 200)
+                    assert.isTrue(response.data.Count > 0, "")
+                    assert.isTrue(response.data.ScannedCount > 0, "")
+                    done()
+                })
+        })
+        it("/API/Users/scan/login", done => {
+            data = null
+            axios.request({
+                    method: "get",
+                    url: baseURL + '/API/Users/scan/login',
+                    data: qs.stringify(data)
+                })
+                .then((response) => {
+                    assert.equal(response.status, 200)
+                    assert.isTrue(response.data.Count > 0, "")
+                    assert.isTrue(response.data.ScannedCount > 0, "")
+                    done()
+                })
+        })
 
-        {
-            description: 'Scan USers',
-            url: baseURL + "/API/Users/new",
-            method: 'get',
-            valid: "ongoing"
-        },
-        {
-            description: 'new user',
-            url: baseURL + "/API/Users/new",
-            method: 'post',
-            data: {
-                a: 1,
-                b: 2
-            },
-            valid: {
-                a: 1,
-                b: 2
+    })
+
+    describe("add users", function () {
+        it("/API/Users/new", done => {
+            let t = new Date()
+            data = {
+                login: "logininput-" + t.toJSON()
             }
-        },
-        {
-            description: 'get user',
-            url: baseURL + "/API/Users/l123",
-            method: 'get',
-            valid: "ongoing"
-        },
-        {
-            description: 'update user',
-            url: baseURL + "/API/Users/l123",
-            method: 'put',
-            valid: "ongoing"
-        },
-        {
-            description: 'delete user',
-            url: baseURL + "/API/Users/l123",
-            method: 'delete',
-            valid: "ongoing"
+            axios.request({
+                    method: "post",
+                    url: baseURL + '/API/Users/new',
+                    data: qs.stringify(data)
+                })
+                .then((response) => {
+                    assert.equal(response.status, 200)
+                    assert.equal(response.data.err, null, "login properly created");
+                    done()
+                })
+        })
+        it("/API/Users/new with null data", done => {
+            data = null
+            axios.request({
+                    method: "post",
+                    url: baseURL + '/API/Users/new',
+                    data: qs.stringify(data)
+                })
+                .then((response) => {
+                    assert.equal(response.status, 200)
+                    assert.equal(response.data, "bad login", "[message]");
+                    done()
+                })
+        })
+    })
+    describe("modify user", function () {
+        let modifiedUser = {
+            "login": "modifieduser"
         }
-    ]
+        beforeEach((done) => {
+            axios.request({
+                    method: "post",
+                    url: baseURL + '/API/Users/new',
+                    data: qs.stringify(modifiedUser)
+                })
+                .then((response) => {
+                    done()
+                })
+            axios.request({
+                    method: "post",
+                    url: baseURL + '/API/Users/new',
+                    data: qs.stringify({
+                        "login": "userToDelete"
+                    })
+                })
+                .then((response) => {
+                    done()
+                })
+        });
+        afterEach((done) => {
+            // axios.request({
+            //         method: "delete",
+            //         url: baseURL + '/API/Users/modifieduser',
+            //     })
+            //     .then((response) => {
+            //         done()
+            //     })
+        })
+        it(" get /API/Users/modifieduser", done => {
+            axios.get(baseURL + '/API/Users/modifieduser')
+                .then((response) => {
+                    assert.equal(response.data.data.login, modifiedUser.login, "[get /API/Users/modifieduser] not successful");
+                    done()
+                })
+        })
+        it("put /API/Users/modifieduser/pwd", done => {
+            let data = {pwd: "abc"}
 
-    looptest.forEach(test=>AxiosQuery(test))
+            axios.request({
+                    url: baseURL + '/API/Users/modifieduser/pwd',
+                    method: 'put',
+                    data: qs.stringify(data)
+                })
+                .then((response) => {
+                    axios.get('/API/Users/modifieduser')
+                    .then ((responseGet)=>{
+                        assert.equal(responseGet.status,200)
+                        assert.isNull(responseGet.data.err)
+                        assert.equal(responseGet.data.pwd,data.pwd)
+                        done()
+
+                    })
+                })
+        })
+        it("put /API/Users/modifieduser/details", done => {
+            let data = {
+                details: {
+                    address: "ici",
+                    email: "no@where.com",
+                    phone: "013235468"
+                }
+            }
+
+            axios.request({
+                    url: baseURL + '/API/Users/modifieduser/details',
+                    method: 'put',
+                    data: qs.stringify(data)
+                })
+                .then((response) => {
+                    assert.equal(response.data.data.login, modifiedUser.login, "[get /API/Users/modifieduser] not successful");
+                    done()
+                })
+        })
+        it("put /API/Users/modifieduser/application", done => {
+            let data = {
+                userApplication: {"ToDo": "Viewer"}
+            } // Object of applicationName:authorisation  (e.g "ToDo": "Viewer"`
+            axios.request({
+                    url: baseURL + '/API/Users/modifieduser/application',
+                    method: 'put',
+                    data: qs.stringify(data)
+                })
+                .then((response) => {
+                    assert.equal(response.data.data.login, modifiedUser.login, "[get /API/Users/modifieduser] not successful");
+                    done()
+                })
+        })
+        it("delete /API/Users/userToDelete", done => {
+            axios.delete(baseURL + '/API/Users/userToDelete')
+                .then((deleteResponse) => {
+                    assert.equal(deleteResponse.status, 200);
+                    assert.isNull(deleteResponse.err);
+                    done()
+                })
+        })
+        it("delete /API/Users/unknownUserToDelete", done => {
+            axios.delete(baseURL + '/API/Users/unknownUserToDelete')
+                .then((deleteResponse) => {
+                    assert.isNotNull(deleteResponse.err, "[unknownUserToDelete] shall fail");
+                    assert.equal(deleteResponse.status, 200);
+                    axios.get(baseURL + '/API/Users/scan')
+                        .then((scanResponse) => {
+                            assert.equal(scanResponse.status, 200);
+                            done()
+                        })
+                })
+        })
+    })
 })
 
 function AxiosQuery(test) {
@@ -52,18 +186,20 @@ function AxiosQuery(test) {
         axios({
                 method: test.method,
                 url: test.url,
+                data: qs.stringify(test.data)
             })
             .then((response) => {
                 assert.equal(response.status, 200, "Query received");
+                // res=JSON.parse(response.data)
                 if (typeof (test.valid) == 'string') {
-                    assert.equal(response.data, test.valid, "Query received");
+                    assert.equal(reponse.data, test.valid, "Query received");
                     done();
                 } else {
                     for (let [key, value] of Object.entries(test.valid)) {
                         assert.isTrue(Object.keys(response.data).includes(key), `[${test.method} Failed]  ---- missing key ${key} in response`);
                         assert.equal(response.data[key], value, `[${test.method} Failed]  ---- expected equality on ${key} to be ${value}`);
                     }
-                    console.log(response.data);
+                    console.log(res.data);
                     done();
                 }
             })
