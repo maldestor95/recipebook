3ebf3628f41e0ae6f7e70fb6661e9ac3ab395225
maldestor95 <maldestor95@gmail.com>
2020-04-02 10:22:46 +0200

    restructure API route

--- app.js
+++ app.js
@@ -73,8 +73,7 @@ app.get("/tot", function (req, res) {
     res.send("Hello World!");
 });
 // app.use("/API/", require("./lib/expensesbdd_router"));
-app.use("/API", require("./lib/Users_router"));
-app.use("/API", require("./lib/Applications_router"));
+app.use("/API", require("./lib/API_router"));
 app.get("/", (req, res) => res.send("Hello toto!"));
 
 app.listen(process.env.PORT || port, () => {
