d2e44287a45616528ef67d7cb96bdf4bb1c711ed
maldestor95 <maldestor95@gmail.com>
2020-06-15 10:10:08 +0200

    image resize functional

--- src/client/src/components/imgupload.vue
+++ src/client/src/components/imgupload.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    {{originSrc}}
     <v-container>
-      <v-row>
+      <v-row v-if="!loaded">
         <form enctype="multipart/form-data">
           <input
             type="file"
@@ -13,28 +14,42 @@
       </v-row>
       <v-dialog
         v-model="dialogEdit"
-        scrollable fullscreen 
-        persistent 
+        scrollable
+        fullscreen
+        persistent
         max-width="500px"
         transition="dialog-transition"
         v-if="originSrc"
       >
         <template v-slot:activator="{ on, attrs }">
-          <img :src="originSrc" alt="original image" class="originsrc"/>
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          Resize
-        </v-btn>
-      </template>
-      <v-card>
-        <img-edit :originSrc="originSrc" @saveModifiedCanvas="save($event)" @cancel="dialogEdit=false"></img-edit>
-      </v-card>
+          <v-container>
+            <v-row>
+              <img
+                :src="originSrc"
+                alt="original image"
+                class="originsrc"
+                id="imguploadPicSrc"
+                width="400"
+              />
+              <v-spacer></v-spacer>
+              <canvas id="finalPic" width="400"></canvas>
+              <v-btn color="warning" @click="drawFinalPic()">Draw</v-btn>
+            </v-row>
+            <v-row>
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">Resize</v-btn>
+              <!-- <v-btn color="info" @click="save()">Apply</v-btn> -->
+              <v-btn color="cancel" @click="cancel()">Cancel</v-btn>
+            </v-row>
+          </v-container>
+        </template>
+        <v-card>
+          <img-edit
+            :originSrc="originSrc"
+            @saveModifiedCanvas="resize($event)"
+            @cancel="dialogEdit=false"
+          ></img-edit>
+        </v-card>
       </v-dialog>
-      
     </v-container>
   </div>
 </template>
@@ -42,10 +57,10 @@
 <script>
 /**
  * Edit input image for croping and size reduction
-   * @vue-prop {String} pictureName - Path to source image  (\\img src="xxx"`)
-   * @vue-event {DataUrl} saveModifiedCanvas - Emit the final canvas as a DataUrl
-   * @vue-event {null} cancel - Emit if cancel   
-   */
+ * @vue-prop {String} pictureName - Path to source image  (\\img src="xxx"`)
+ * @vue-event {DataUrl} ApplyImage - Emit the final canvas as a DataUrl
+ * @vue-event {null} cancel - Emit if cancel
+ */
 import imgEdit from "./imgedit";
 export default {
   components: {
@@ -55,8 +70,8 @@ export default {
     pictureName: {
       type: String,
       default: null,
-      required:true
-    },
+      required: true
+    }
   },
   data() {
     return {
@@ -65,8 +80,8 @@ export default {
       originSrc: null,
       originFileSize: null,
       originFileName: null,
-      dialogEdit:false,
-    
+      dialogEdit: false,
+      loaded: false
     };
   },
   methods: {
@@ -75,23 +90,45 @@ export default {
       URL.revokeObjectURL(this.originSrc);
 
       this.originSrc = URL.createObjectURL(files[0]);
-
       this.originFileName = files[0].name;
       this.originFileSize = files[0].size + " bytes";
+      this.loaded = true;
     },
+    drawFinalPic() {
+      let canvas = document.getElementById("finalPic");
+      let canvasOrigin=document.getElementById('imguploadPicSrc')
+      let ctx = canvas.getContext("2d");
+      let img = new Image();
+      img.src = this.originSrc;
+      let _this = this;
+      canvas.height=canvasOrigin.height*canvas.width/canvasOrigin.width;
 
-    save(data) {
-      // let ctx = document.getElementById("destcanvas");
-
-      // this.imageURI = ctx.toDataURL("image/jpeg", 1);
-      // this.$emit("saveModifiedCanvas", ctx);
-
-      // this.finalFileName = "thumb" + this.originFileName;
-      // this.finalFileSize = this.imageURI.length + " bytes";
-      // this.saved = true;
-     this.originSrc=data
-    //  .toDataUrl("image/jpeg", 1)
-     this.resize=false
+      img.onload = function() {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        // ctx.drawImage(img,0,0,canvasOrigin.width,canvasOrigin.height,0,0,ctx.width,ctx.height)
+        canvas.toBlob(
+          blob => {
+            // eslint-disable-next-line no-console
+            console.log(blob);
+            _this.$emit("ApplyImage", blob);
+          },
+          "image/jpg",
+          1
+        );
+      };
+    },
+    save() {
+      this.$emit("ApplyImage", this.originSrc);
+    },
+    resize(dataUrl) {
+      this.originSrc = dataUrl;
+      this.dialogEdit = false;
+    },
+    cancel() {
+      this.loaded = false;
+      this.files = [];
+      this.name = "";
+      this.originSrc = null;
     }
   }
 };
