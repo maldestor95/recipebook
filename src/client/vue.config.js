3037a3e7a9e638fec1fb0a3d66a4afdac37d3876
Ludovic Deparis <maldestor95@gmail.com>
2020-08-22 11:16:48 +0200

    configure Webpack for vuetify resolution
    required for cross referencing vuetify
    
    https://stackoverflow.com/questions/59218127/error-while-moving-component-from-application-to-a-library#new-answer

--- src/client/vue.config.js
+++ src/client/vue.config.js
@@ -1,5 +1,6 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 //   .BundleAnalyzerPlugin;
+const path = require("path");
 
 module.exports = {
   "transpileDependencies": [
@@ -9,14 +10,12 @@ module.exports = {
   runtimeCompiler: true, //https: //cli.vuejs.org/config/#runtimecompiler
   configureWebpack: {
     // plugins: [new BundleAnalyzerPlugin()],
-      devtool: 'source-map',
-      // module: {
-      //   rules: [{
-      //     test: /\.(woff|woff2|eot|ttf|otf|png|jpe?g)$/,
-      //     use: [
-      //       'file-loader',
-      //     ],
-      //   }]}
+    resolve: {
+      alias: {
+        /* Use vuetify from the app, not from fence-vue */
+        vuetify: path.resolve(__dirname, "node_modules/vuetify"),
+      },
+    }
   },
   devServer: {
     proxy:  'http://localhost:3000/',
@@ -25,9 +24,9 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: `@import "@/styles/variables.scss";`
-      }
-    }
+        prependData: `@import "@/styles/variables.scss";`,
+      },
+    },
   },
-  publicPath:'/'
-}
+  publicPath: "/",
+};
