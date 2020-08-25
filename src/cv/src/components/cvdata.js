1bc2b5c342b6e47827e13f9aa381ca1a2453137a
maldestor95 <maldestor95@gmail.com>
2020-08-25 14:07:39 +0200

    update cv

--- src/cv/src/components/cvdata.js
+++ src/cv/src/components/cvdata.js
@@ -3,7 +3,7 @@ export default {
   cv: {
     nom: 'Ludovic Deparis',
     mission: 'Chef de Projet',
-    about: '<p>Vingt années passées dans le développement de systèmes embarqués m’ont permis de travailler sur de nombreuses problématiques techniques et organisationnelles.</p> <ul><li>La première moitié  de mon expérience a eu lieu dans le secteur des Télécom où j\'ai successivement  conçu et mis au point la partie RF d\'amplificateurs pour l\'infrastructure 2G et 3G puis l\'électronique de 6 téléphones mobiles.</li><li>Après 10 années de développement hardware, j\'ai conduits plusieurs projets de systèmes embarqués, depuis la conception matérielle et logicielles jusqu\'à la maintenance applicative. Je compte parmi ces projets: <ul><li>une Radio Logicielle 300MHz à 3GHz</li><li>Outils de tests et conformance NFC et Wireless Charging</li><li>Drones</li></ul></li></ul></p>Manager de proximité, j\'aime faire progresser mon équipe et intégrer des outils pour améliorer la qualité des livrables des projets et augmenter la productivité. Passionné d\'informatique, j\'ai conçu des applications pour:<p><ul><li>  Automatiser les tests en Python</li><li> Piloter par smartphone les systèmes embarqués</li><li>Automatisation de rapport d\'avancement</li> Ces expériences m\'ont permis de démontrer une adaptabilité très forte aux évolutions techniques (Télécom, drones, outils NFC, web, Cloud..)..<br>Mon aisance en anglais est appréciée tout autant que mon enthousiasme et ma rigueur.<br>Je fais monter mes collaborateurs en compétences et prends part au développement logiciel ainsi qu’ à la validation du projet.',
+    about: '<p>Vingt années passées dans le développement de systèmes embarqués m’ont permis de travailler sur de nombreuses problématiques techniques et organisationnelles:</p> <ul><li>10 ans dans le secteur des Télécom où j\'ai successivement  conçu et mis au point la partie RF d\'amplificateurs pour l\'infrastructure 2G et 3G puis l\'électronique de 6 téléphones mobiles.</li><li>10 ans de conduite de projets de conception de produits électroniques, depuis la conception matérielle et logicielles jusqu\'à la maintenance applicative. Je compte parmi ces projets: <ul><li>une Radio Logicielle 300MHz à 3GHz</li><li>des outils de tests et conformance NFC et Wireless Charging</li><li>des évolutions applicative du drone ANAFI de Parrot</li></ul></li></ul></p>Manager de proximité, j\'aime faire progresser mon équipe et intégrer des outils pour améliorer la qualité des livrables des projets et augmenter la productivité. Passionné d\'informatique, j\'ai conçu des applications tel que l\'automatisation de tests ou la génération de rapport d\'avancement ou encore pour piloter les systèmes embarqués par smartphone</p><p>Ces expériences m\'ont permis de démontrer une adaptabilité très forte aux évolutions techniques (Télécom, drones, outils NFC, web, Cloud..)..<br>Mon aisance en anglais est appréciée tout autant que mon enthousiasme et ma rigueur.<br>Je fais monter mes collaborateurs en compétences et prends part au développement logiciel ainsi qu’ à la validation du projet.</p>',
     exp: [{
       mission: 'Chef de projet drone ANAFI',
       date: '2018-2020',
@@ -11,8 +11,9 @@ export default {
       location: 'PARIS',
       objectifs: 'Assurer la maintenance applicative et matérielle du drone ANAFI sorti début juillet 2018. J\'ai pris en charge le support niveau 2 et organisé les développements des évolutions logicielles notamment celle d\'une version applicative Photogrammétrie et d\'une version Défense Sécurité ',
       tasks: [
-        ' Conception d’un outil de validation Express.js+Vue.js du plan de vol (extraction de métadonnées des images et positionnement sur Mapbox)',
         ' Pilotage de la R&D (Software, validation et qualification Hardware) et coordination avec les services Support, Achat, Production, Industrialisation, Commerce, Legal',
+        ' Coordination des développements de photogrametrie avec PIX4D',
+        ' Conception d’un outil de validation Express.js+Vue.js du plan de vol (extraction de métadonnées des images et positionnement sur Mapbox)',
         ' Support technique pour les clients Défense et Sécurité (France, Suisse, USA)'
       ],
       pics: ['ANAFIWork.jpg']
@@ -24,7 +25,7 @@ export default {
       location: 'VILLENEUVE-D’ASCQ',
       objectifs: 'Management de la R&D composée de 9 ingénieurs et techniciens (logiciel embarqué, FPGA, électronique) dans le développement d\'outils de tests pour le NFC et chargement sans contact(Qi, AirFuel Resonant)',
       tasks: [
-        ' Participation aux comités normatif Qi en Italie, USA et Japon pour soutenir notre testeur',
+        ' Soutenance de notre testeur QI aux comités normatif Qi en Italie, USA et Japon ',
         ' Interface opérationnelle avec le comité AirFuel Resonant',
         ' Gestion du budget et suivi des compétences- 1.1M€/an sur 8 projets (5 en maintenance, 3 en développement)',
         ' Gestion de projet',
@@ -33,11 +34,11 @@ export default {
       pics: ['mp500tcL38wpcTester.jpg']
     },
     {
-      mission: 'Chef de projet communications sans fil (2G,3G,4G)',
+      mission: 'Chef de projet communications sans fil',
       date: '2010-2014',
       company: 'TIMES REVERSAL COMMUNICATIONS',
       location: 'CERGY PONTOISE',
-      objectifs: '',
+      objectifs: 'Management fonctionnel de 10 ingénieurs pour le développements des outils de tests 2G,3G,4G',
       tasks: [
         ' Systèmes embarqués pour Sagem Wireless, ST - Ericsson et Ministère de la Défense',
         ' Gestion d\'obsolescence, transfert de site de production',
@@ -52,7 +53,7 @@ export default {
       date: '2002-2009',
       company: 'SAGEM MOBILES',
       location: 'CERGY-PONTOISE',
-      objectifs: '',
+      objectifs: 'Définir les moyens et organiser le développement de la partie radio de plusieurs téléphones mobiles pour SAGEM MOBILES',
       tasks: [
         ' Conception de la Radio de 7 téléphones mobiles, volume moyen de 400K pièces par an',
         ' Automatisation du banc de réglage radio en Python',
@@ -68,10 +69,11 @@ export default {
       company: 'FILTRONIC COMTEK',
       location: 'SHIPLEY(UK)',
       summary: '',
-      objectifs: '',
+      objectifs: 'Conception d\'amplificateurs RF pour Station de Base GSM',
       pics: ['amplifiers.jpg', 'awr.png'],
       tasks: [
-        ' Conception d’amplificateurs RF pour Station de Base GSM',
+        ' Etude système avec réalisation de simulations (Microwave Office), du schéma et du routage ',
+        ' Mise au point des cartes électroniques (Low Noise Amplifier utilisant des composants discrets sur PCB Microstrip)',
         ' Automatisation du paramétrage du logiciel de simulation (VBA)'
       ]
     },
@@ -155,9 +157,19 @@ export default {
       value: 3
     },
     {
+      nom: 'RF(800-2500MHz)',
+      groupe: 'Hardware',
+      value: 4
+    },
+    {
       nom: 'Conception mécanique',
       groupe: 'Hardware',
       value: 3
+    },
+    {
+      nom: 'Francais (langue maternelle)',
+      groupe: 'Langues',
+      value: 5
     },
     {
       nom: 'Anglais (famille bilingue)',
@@ -178,7 +190,20 @@ export default {
         groupe: 'Diplôme',
         annee: '1997-1998',
         topic: ' MSc (Master of Science) en automatique et traitement du signal à Manchester (UK)'
+      },
+      {
+        nom: 'ISTQB Fundation',
+        groupe: 'Diplôme',
+        annee: 'Prévu sur Q4 2020',
+        topic: 'Qualification standardisée pour le test logiciel'
+      },
+      {
+        nom: 'Prince2 Fundation',
+        groupe: 'Diplôme',
+        annee: 'Prévu sur Q4 2020',
+        topic: ' Méthodologie projets'
       }
+
     ]
   }
 }
