33b2bcb33d6d18ec75e3ac5b72033335698136b7
maldestor95 <maldestor95@gmail.com>
2020-10-06 08:44:25 +0200

    test(auth): authentication middleware

--- src/server/src/route/auth.ts
+++ src/server/src/route/auth.ts
@@ -1,60 +1,22 @@
 /** Express router providing user related routes
  * @module routers/auth
  */
-import express from 'express'
+import express, { NextFunction } from 'express'
 import * as definition from '../lib/definition'
 
 export const router = express.Router();
 
 
-
-/**
- * Route redirection if there is no session linked to the user
- * @name get/nosession
- * @function
- * @static
- * @memberof module:routers/auth
- * @param {string} path - Express path
- * @param {callback} middleware - Express middleware.
- */
-router.get('/noSession', (req, res) => {
-    var context = req.app.locals.specialContext;
-    req.app.locals.specialContext = null;
-    res.send('no active session while connecting '+context)
-})
-/**
- * Route redirection if there is the user is not authorised to access the route
- * @name get/notAuthorised
- * @function
- * @static
- * @param {string} path - Express path
- * @param {callback} middleware - Express middleware.
- */
-router.get('/notAuthorised', (req, res) => {
-    var context = req.app.locals.specialContext;
-    req.app.locals.specialContext = null;
-    res.send('not authorized to access  '+context)
-
-})
-/**
- * checkLevelClearance to access the userApplication with level
- * @name checkLevelClearance
- * @inner
- * @param {Object} req Express middleware
- * @param {String} topic (voir definition.js)
- * @param {String} role (voir definition.js)
- */
-export const  checkLevelClearance = function (req:express.Request, topic:definition._application,role:definition._role) {
+const checkLevelClearance = function (req: express.Request, topic: definition._application, role: definition._role) {
     if (req.session!.hasOwnProperty('passport')) {
         if (Object.keys(req.session!.passport.user.userApplication).includes(topic)) {
-            let userLevel=req.session!.passport.user.userApplication[topic]
-            if (role==null) {return userLevel}
-            else
-            {
-                let tempdef=Object.keys(definition._role)
-                let r=tempdef.indexOf(role)+1
-                let temprole=tempdef.splice(0,r)
-                let rr= temprole.includes(userLevel)
+            let userLevel = req.session!.passport.user.userApplication[topic]
+            if (role == null) { return userLevel }
+            else {
+                let tempdef = Object.keys(definition._role)
+                let r = tempdef.indexOf(role) + 1
+                let temprole = tempdef.splice(0, r)
+                let rr = temprole.includes(userLevel)
                 return rr
             }
         } else return false
@@ -84,11 +46,11 @@ export const  checkLevelClearance = function (req:express.Request, topic:definit
  * 
  * .put(recetteAuthEditor, (req, res) => { ...}
  */
-export const  isAuthorized=function(req:express.Request,res:express.Response,next:express.NextFunction,applicationName:definition._application,minimumLevelRequired:definition._role){
+export const isAuthorized = function (req: express.Request, res: express.Response, next: express.NextFunction, applicationName: definition._application, minimumLevelRequired: definition._role) {
     if (checkLevelClearance(req, applicationName, minimumLevelRequired)) {
         next()
     } else {
-        res.status(403).send('notAuthorised')  // TODO add meaningful text
+        res.status(401).send('not authorised')  // TODO add meaningful text
     }
 }
 /**
@@ -100,18 +62,18 @@ export const  isAuthorized=function(req:express.Request,res:express.Response,nex
  * @param {Object} next Express middleware
  * @param {String} userApplicationName Application Name such as "Recettes"
  */
-export const checkAuth = function (req:express.Request,res:express.Response,next:express.NextFunction,applicationName:definition._application) {
-    if (!req.session!.passport) {
-        req.app.locals.specialContext = req.path
-        res.redirect('/noSession')
-    } else {
+export const checkAuth = function (req: express.Request, res: express.Response, next: NextFunction, applicationName: definition._application) {
+    if (!req.session!.passport) return res.status(401).send('no active session')
+    try {
         if (Object.keys(req.session!.passport.user.userApplication).includes(applicationName)) {
             next()
         } else {
-            req.app.locals.specialContext = req.path
-            res.redirect('/notAuthorised')
+            res.status(401).send('not authorised')
         }
+    } catch (error) {
+        res.status(401).send('not authorised')
     }
+
 }
 
 
