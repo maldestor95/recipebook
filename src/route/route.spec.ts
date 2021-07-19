c67fd6d5fd0468f7e8d54fa5b607c1722ae603d6
maldestor95 <maldestor95@gmail.com>
2021-07-19 07:18:04 +0200

    feat(route): first route testable

--- src/route/route.spec.ts
+++ src/route/route.spec.ts
@@ -1,87 +1,20 @@
+
 import chai, { expect } from 'chai'
 import chaiHttp from "chai-http"
 chai.use(chaiHttp);
-import express from "express";
-import {createServer} from "http"
 
-import route from './route'
-
-const testServer = express();
-const initapp=(newport:number)=>{   
-    const port = 3000 | newport ;
-    testServer.set('port', port);
-    // app.use('/fr',route)
-    var server = createServer(testServer);
-    server.listen(port,()=>{
-        console.log(`server has started on port:${port} at ${new Date().toLocaleTimeString()}`)
-        if (process.env.NODE_ENV) {
-          console.log(`environment mode: ${process.env.NODE_ENV} `)
-        }
-        
-      });
-      server.on('error', onError);
-      server.on('listening', onListening);
-      function onError(error:any) {
-        if (error.syscall !== 'listen') {
-          throw error;
-        }
-      
-        var bind = typeof port === 'string'
-          ? 'Pipe ' + port
-          : 'Port ' + port;
-      
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-          case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-          case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            // process.exit(1);
-            break;
-          default:
-            throw error;
-        }
-      }
-      function onListening() {
-        var addr = server.address();
-        if (addr){
-      
-          var bind = typeof addr === 'string'
-          ? 'pipe ' + addr
-          : 'port ' + addr.port;
-          console.log('Listening on ' + bind);
-        }
-      }
-}
-const testServerPort = 3001
-const testServerAddress = `http://localhost:${testServerPort}`;
 describe("test route", function () {
-    before(()=>{
-        initapp(testServerPort)
-        testServer.use('/test',route)
-    })
-    it("dir all", done => {
-        expect(false, 'response is not an object');
-            // console.log(data)
-            done()
-    })
-    it("shall request", async ()=> {
-        await chai.request(testServerAddress)
-        .get('/test/fl')
-        .then(data => {
-            console.log(data)
-            expect(data.status).to.eq(200)
-            const result = data.body
-            // expect(result.Items.length).to.eq(4)
-            // expect(result.Count).to.eq(4)
-            
-        })
-        .catch(err => {
-            console.log(err)
-            expect(err).to.be.null
-        })
+    before( ()=>{})
+    after(()=>{})
+    beforeEach(()=>{ })
+    it("shall async get", (done) => {
+        // console.log(tServer.Address)
+        chai.request('http://localhost:3000')
+        .get('/fr/fl')
+        .then(data=>{console.log(data.text);done()})
+        .catch(err=> {console.log(err);done()})
+        expect(true);
     })
 
-})
+
+    })
--- src/route/route.ts
+++ src/route/route.ts
@@ -1,9 +1,16 @@
 "use strict"
 import express from 'express';
 var frouter = express.Router();
-
+// frouter.use(express.json);
+frouter.all('*',(req,res,next)=>{
+    console.log('req,res')
+    next()
+})
 frouter.get('/fl',(req,res)=>{
     res.send('ok')
 })
-
+frouter.post('/setfolder', (req,res)=>{
+    console.log(req.body.name)
+    res.send('setfolder')
+})
 export = frouter;
