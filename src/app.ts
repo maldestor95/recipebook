b09bc34131fccc52c7b3917646e0a85010d35670
Ludovic Deparis <maldestor95@gmail.com>
2021-08-25 09:45:10 +0200

    feat(cli): initialise cli

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,41 @@
+#!/usr/bin/env node
 
- import * as RECIPE from './recipe';
-
-// let recipeList = new RECIPE.Ingredients()
-
-// recipeList.addIngredient({ingredient:'tomates',qty:12});
-// recipeList.addIngredient({ingredient:'oignons',qty:5});
-// recipeList.addIngredient({ingredient:'sel',qty:'une pincée'});
-
-// console.log(recipeList.list)
-// console.log(recipeList.ingredientsToYAML())
-
-const recette=new RECIPE.Recipe()
-
-type menuType = {
-    id:number,
-    text: string,
-    cb:void|null
-}
-const menu:menuType[] = [
-    {id:4,text:'Set Recipe Name',cb:null},
-    {id:3,text:'show Ingredients',cb:null},
-    {id:2,text:'Add Ingredients',cb:null},
-    {id:1,text:'Create New Recipe',cb:null},
-    {id:0,text:'Quit', cb:null},
-]
-function showMenu():void{
-    menu.forEach(element => {
-        console.log(`${element.id}: ${element.text}`)
-    });
+const cb = function () {
+    console.log("cactus")
 }
 
-showMenu()
-
-process.stdin.on('data', data => {
-    const dataString = data.toString().slice(0, -2)
-    const choice = <number><unknown> dataString
-    switch (choice) {
-        case 0:
-            console.log('bye')
-            process.exit();            
-            break;
-        default:
-
-            break;
-    }
-    // showMenu();
-});
+import yargs from "yargs"
+yargs
+    .scriptName('Recipe Creator')
+    .usage('$0 <cmd> [args]')
+    .command('new-recipe <name> <filename>', 'welcome ter yargs!', (yargs) => {
+        yargs.positional('name', {
+            type: 'string',
+            default: null,
+            describe: 'name of the recipe in quotes'
+        })
+        yargs.positional('filename', {
+            type: 'string',
+            default: null,
+            describe: 'filename without spaces and no extension'
+        })
+    }, function (argv) {
+        console.log('Creation of ', argv.name, 'which correspond to', argv.filename)
+    })
+    .command('update-list <filename>', 'update recipe list', (yargs) => {
+        yargs.positional('filename', {
+            type: 'string',
+            default: null,
+            describe: 'filename without spaces and no extension'
+        })
+    }, function (argv) {
+        console.log('update list with ', argv.filename)
+    })
+    .command({
+        command: '*',
+        handler() {
+            yargs.showHelp()
+        }
+    })
+    .help()
+    .argv
