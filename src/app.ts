21c450f00189eddebd47883a67f70574f2f61d95
maldestor95 <maldestor95@gmail.com>
2021-10-05 18:39:02 +0200

    feat(validate): validate recipe folder

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
 #!/usr/bin/env node
 
 import { validaterecipe } from './validaterecipe'
+import * as fs from 'fs/promises'
 
-console.log(validaterecipe('recipe/taboule.md'))
+// console.log(validaterecipe('recipe/taboule.md'))
+// validaterecipe('recipe/truiteaufour.md').then((r)=>{console.log(r)})
+// 
+
+    	const recipePath='recipe/'
+    fs.readdir(recipePath)
+  
+    // If promise resolved and
+    // datas are fetched
+    .then(filenames => {
+        for (let filename of filenames) {
+            validaterecipe(`${recipePath}${filename}`)
+            .then((result)=>{console.log(filename, result)})
+            .catch((err)=>{console.log(filename,err)})
+        }
+    })
+  
+    // If promise is rejected
+    .catch(err => {
+        console.log(err)
+    })
--- src/validaterecipe.ts
+++ src/validaterecipe.ts
@@ -1,4 +1,8 @@
+import { promises } from "dns";
 import { readFile } from "fs/promises";
+import * as Joi from 'joi';
+import { string } from "joi";
+import * as Yaml from 'yaml'
 
 const convertMarkdown= function(mdData:string):{ok:boolean,yml:string|null,md:string|null} {
     const errorMsg:{ok:boolean,yml:string|null,md:string|null}={ok:false,yml:null,md:null}
@@ -18,28 +22,37 @@ const convertMarkdown= function(mdData:string):{ok:boolean,yml:string|null,md:st
 
     return {ok:true,yml,md}
 }
-const recipeYAMLvalidation = function (yml:string): void  {
-
+const recipeYAMLvalidation = function (yml:string|null): Joi.ValidationError|undefined  {
+    const parsedYML=Yaml.parse(<string>yml)
+    const schema = Joi.object({
+        title: Joi.string(),
+        link: Joi.string(),
+        ingredients: Joi.array().items({
+            ingredient:Joi.string(),
+            qty:[Joi.string(),Joi.number()]
+        })
+            .min(1)
+    })
+    const valid=schema.validate(parsedYML)
+    return valid.error
 }
-export const  validaterecipe=function(filename:string):Boolean {
+export const  validaterecipe=async function(filename:string):Promise<Joi.ValidationError|{err:string}|boolean> {
     //open file
-    readFile(filename,{encoding:'utf-8'})
+    const response = await readFile(filename,{encoding:'utf-8'})
     .then((fileData:string)=>{
         //convert file
         const parsedFile = convertMarkdown(fileData)
-        if (parsedFile.ok==false) return false
+        if (parsedFile.ok==false) throw {err:`couldn\'t parse file ${filename}`}
         //validation yml
-            // title
-            // link
-            // ingredients
-            console.log(parsedFile.yml)
-        // description in markdown not empty
-
+        const valid=recipeYAMLvalidation(parsedFile.yml)           
+        if  (valid==undefined) return true
+        throw valid
     })
     .catch((err)=>{
-        console.log(err)
-        return(false)
+        // console.log(err)
+        // throw {err: `couldn\'t open file ${filename}`}
+        return err
     })
-    return true;
+    return response
 }
 
