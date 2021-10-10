6a2a5adabd5b38d75450c9a9ac02d87b2c5dd9dc
maldestor95 <maldestor95@gmail.com>
2021-10-10 09:13:37 +0200

    refactor(validate): constants.js

--- src/validaterecipe.ts
+++ src/validaterecipe.ts
@@ -1,12 +1,9 @@
-// import { promises } from "dns";
-import { readFile } from "fs/promises";
+import { readFile, readdir } from "fs/promises";
 import * as Joi from 'joi';
-// import { string } from "joi";
 import * as Yaml from 'yaml'
-import * as fs from 'fs/promises'
+import constants from './constants'
 
-const recipePath='recipe/'
-type recipeValidationType = Joi.ValidationError|{err:string}|boolean
+type recipeValidationType = Joi.ValidationError|{err:string}|boolean|{yml?:string|null,md?:string|null}
 const convertMarkdown= function(mdData:string):{ok:boolean,yml:string|null,md:string|null} {
     const errorMsg:{ok:boolean,yml:string|null,md:string|null}={ok:false,yml:null,md:null}
 
@@ -40,7 +37,11 @@ const recipeYAMLvalidation = function (yml:string|null): Joi.ValidationError|und
     return valid.error
 }
 
-const  validaterecipe=async function(filename:string):Promise<recipeValidationType> {
+type validateRecipeOption = {
+    yml? :boolean,
+    md?: boolean,
+}
+const  validaterecipe=async function(filename:string,option?:validateRecipeOption):Promise<recipeValidationType> {
     return new Promise((resolve,reject)=>{
         const response = readFile(filename,{encoding:'utf-8'})
         .then((fileData:string)=>{
@@ -49,7 +50,11 @@ const  validaterecipe=async function(filename:string):Promise<recipeValidationTy
             if (parsedFile.ok==false) reject ({err:`couldn\'t parse file ${filename}`})
             //validation yml
             const valid=recipeYAMLvalidation(parsedFile.yml)           
-            if  (valid==undefined) resolve(true)
+            if  (valid==undefined) {
+                if (option== undefined )resolve(true)
+                if (option?.yml) resolve({yml:parsedFile.yml})
+                if (option?.md) resolve({md:parsedFile.md})
+            }
             reject(valid)
         })
         .catch((err)=>{
@@ -59,11 +64,11 @@ const  validaterecipe=async function(filename:string):Promise<recipeValidationTy
 }
 export const validaterecipefolder= async function():Promise<boolean>{
     let validaterecipePromise:Array<Promise<recipeValidationType>>=[]
-    await fs.readdir(recipePath)
+    await readdir(constants.recipePath)
     .then(filenames => {
         for (let filename of filenames) {
             if (filename!='recettelist.md')
-            validaterecipePromise.push(validaterecipe(`${recipePath}${filename}`))
+            validaterecipePromise.push(validaterecipe(`${constants.recipePath}${filename}`))
         }
     })
     .catch(err => {
@@ -80,3 +85,53 @@ export const validaterecipefolder= async function():Promise<boolean>{
     
     return conclusion
 }
+
+export const createRecipeArray= async function ():Promise<Array<{title:string, link:string}>> {
+    let recipeArrayPromise:Array<Promise<recipeValidationType>>=[]
+    await readdir(constants.recipePath)
+    .then(filenames => {
+        for (let filename of filenames) {
+            if (filename!='recettelist.md')
+            recipeArrayPromise.push(validaterecipe(`${constants.recipePath}${filename}`,{yml:true}))
+        }
+    })
+    .catch(err => {
+        throw(err)
+    })
+    
+    let conclusion:Array<{title:string, link:string}> = []
+
+
+    await Promise.all(recipeArrayPromise)
+    .then(ymlstringArray=>{
+        let ymldata=ymlstringArray.map(ymlstring=>{
+            console.log(ymlstring)
+            const parsedYML= Yaml.parse(<string>ymlstring)
+            return {title:parsedYML.title, link:parsedYML.link}
+        })
+        return ymldata
+    
+    })
+    .then(res=> {console.log(res)})
+    .catch((err=>{ console.log(err)
+        // conclusion= false
+    }))
+    
+    return conclusion
+}
+/*export const generateRecipelist=async function():Promise<resultType> {
+    return new Promise((resolve, reject) => {
+        validaterecipefolder()
+        .then((status)=>{
+            if (status==false) reject ({err:"validation of recipes showed error"})// create table while parsing file
+        })
+        .then((datatable)=>{
+            //  generate file
+            resolve({data:"ok",err:null})
+        })
+        .catch((err:resultType)=>{
+            reject(err)
+        })
+    });
+}
+*/
